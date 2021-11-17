54bca6933f6e8b3b01ccd513e35093c1f8f567b8
Vlad <vladzbr1@gmail.com>
2021-11-17 14:38:17 -0500

    added

--- national_park_backend/national_park_api/models.py
+++ national_park_backend/national_park_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class NationalPark(models.Model):
+    description = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    admission_fee = models.FloatField()
--- national_park_backend/national_park_api/views.py
+++ national_park_backend/national_park_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .serializers import NationalParkSerializer
+from .models import NationalPark
+
+class NationalParkList(generics.ListCreateAPIView):
+    queryset = NationalPark.objects.all().order_by('id')
+    serializer_class = NationalParkSerializer
+
+class NationalParkDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = NationalPark.objects.all().order_by('id')
+    serializer_class = NationalParkSerializer
